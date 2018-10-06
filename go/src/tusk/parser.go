4a2cbe8c1e1243598c6678fd2dca1738a40399a2
Joshua Strobl <joshua@stroblindustries.com>
2018-10-06 04:26:05 +0300

    Ensure we deduplicate our Channels list on invite.

--- go/src/tusk/parser.go
+++ go/src/tusk/parser.go
@@ -56,6 +56,7 @@ func OnInvite(c *girc.Client, e girc.Event) {
 	c.Cmd.Join(channel)
 
 	Config.Channels = append(Config.Channels, channel)
+	Config.Channels = DeduplicateList(Config.Channels)
 	SaveConfig()
 }
 
